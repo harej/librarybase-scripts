577f7f7317551577945c9b08eb3b031b1f9d58d2
harej <jamesmhare@gmail.com>
2018-07-29 10:48:36 -0700

    More parallel processes

--- codeswitch_update.py
+++ codeswitch_update.py
@@ -1,11 +1,18 @@
 import arrow
+import math
 import redis
 import requests
 import threading
+import time
 from site_credentials import *
 
+manifest = ['P356', 'P698', 'P932']
+read_threads = 9
+
 REDIS = redis.Redis(host=redis_server, port=redis_port, password=redis_key)
+THREAD_LIMIT = 2 + len(manifest) + read_threads
 today = arrow.utcnow().format('YYYYMMDD')
+limit = {}
 
 def process_blob(prop, blob):
     if '@graph' not in blob:
@@ -26,28 +33,45 @@ def process_blob(prop, blob):
             wd_item,
             prop_value)
 
-def get_and_process_blob(prop, counter):
+def get_blob(prop, counter):
     url = ('https://query.wikidata.org/bigdata/ldf?subject=&predicate='
            'http%3A%2F%2Fwww.wikidata.org%2Fprop%2Fdirect%2F{0}&object='
            '&page={1}')
 
     print(prop, '-', str(counter))
-    r = requests.get(
-        url.format(prop, counter),
-        headers={"Accept": "application/ld+json"})
+
+    while True:
+        r = requests.get(
+                url.format(prop, counter),
+                headers={"Accept": "application/ld+json"})
+        if 'Rate limit exceeded' in r.text:
+            time.sleep(10)
+        else:
+            break
 
     try:
-        blob = r.json()
+        return r.json()
     except:
-        # If it doesn't work, most likely the server returned a 500 error,
-        # which it does in HTML for some reason. In any case, we're done.
-        return False
+        print(r.text)
 
-    # Processing blob. A return of False means that despite being valid
-    # JSON, the content we want is not there.
-    if process_blob(prop, blob) is False:
-        return False
+class GetAndProcessBlob(threading.Thread):
+    def __init__(self, threadID, name, prop, counter):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.prop = prop
+        self.counter = counter
 
+    def run(self):
+        prop = self.prop
+        counter = self.counter
+
+        if prop in limit:
+            if counter > limit[prop]:
+                return False
+
+        blob = get_blob(prop, counter)
+        process_blob(prop, blob)
 
 class CreateCache(threading.Thread):  # gotta go fast!
     def __init__(self, threadID, name, prop):
@@ -57,12 +81,39 @@ class CreateCache(threading.Thread):  # gotta go fast!
         self.prop = prop
 
     def run(self):
+        global limit
         prop = self.prop
-        counter = 1
 
+        blob = get_blob(prop, 1)
+        process_blob(prop, blob)
+
+        for entry in blob['@graph']:
+            if 'void:triples' in entry:
+                limit[prop] = int(math.ceil(entry['void:triples'] / 100))
+
+        # At this point, if `limit[prop]` is not defined, something is seriously
+        # wrong.
+
+        if prop not in limit:
+            raise Exception
+
+        counter = 2
+
+        # Time to iterate through the rest of the pages up to the limit
         while True:
-            if get_and_process_blob(prop, counter) is False:
+            if counter > limit[prop]:
                 break
+
+            while threading.active_count() >= THREAD_LIMIT:
+                time.sleep(0.25)
+
+            thread = GetAndProcessBlob(
+                counter,
+                'thread-' + prop + '-' + str(counter),
+                prop,
+                counter)
+
+            thread.start()
             counter += 1
 
         REDIS.rename(
@@ -72,9 +123,9 @@ class CreateCache(threading.Thread):  # gotta go fast!
             'wikidata_to_{0}_{1}'.format(prop, today),
             'wikidata_to_{0}'.format(prop))
 
-def main():
+def main(manifest):
     thread_counter = 0
-    for prop in ['P356', 'P698', 'P932']:
+    for prop in manifest:
         thread = CreateCache(
             thread_counter,
             'thread-' + str(thread_counter),
@@ -83,4 +134,4 @@ def main():
         thread_counter += 1
 
 if __name__ == '__main__':
-    main()
+    main(manifest)
