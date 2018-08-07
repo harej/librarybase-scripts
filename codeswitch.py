be1b5f9541510e8aee37c5422eb851f375310c4f
harej <jamesmhare@gmail.com>
2018-08-07 09:23:18 -0700

    Removing date suffix

--- codeswitch.py
+++ codeswitch.py
@@ -6,7 +6,7 @@ REDIS = redis.Redis(host=redis_server, port=redis_port, password=redis_key)
 def identifier_generator(wd_prop, list_name, descending_order=True):
     REDIS.delete(list_name)
 
-    identifier_list = REDIS.hgetall(wd_prop + '_to_wikidata_20180804')
+    identifier_list = REDIS.hgetall(wd_prop + '_to_wikidata')
     identifier_list = [y.decode('utf-8') + '|' + x.decode('utf-8') for x, y in identifier_list.items()]
     identifier_list.sort(reverse=descending_order)
     identifier_list = [x.split('|')[1] for x in identifier_list]
@@ -28,19 +28,19 @@ def hget(keyname, itemname):
         return raw.decode('utf-8')
 
 def doi_to_wikidata(doi):
-    return hget('P356_to_wikidata_20180804', doi)
+    return hget('P356_to_wikidata', doi)
 
 def pmid_to_wikidata(pmid):
-    return hget('P698_to_wikidata_20180804', pmid)
+    return hget('P698_to_wikidata', pmid)
 
 def pmcid_to_wikidata(pmcid):
     return hget('P932_to_wikidata', pmcid)
 
 def wikidata_to_doi(wikidata):
-    return hget('wikidata_to_P356_20180804', wikidata)
+    return hget('wikidata_to_P356', wikidata)
 
 def wikidata_to_pmid(wikidata):
-    return hget('wikidata_to_P698_20180804', wikidata)
+    return hget('wikidata_to_P698', wikidata)
 
 def wikidata_to_pmcid(wikidata):
     return hget('wikidata_to_P932', wikidata)
