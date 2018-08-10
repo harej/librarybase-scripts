c6548d632a81fa2930001905de62cb9832d65a57
harej <jamesmhare@gmail.com>
2018-08-09 21:26:15 -0700

    Rewrite doi_to_pmcid to use Codeswitch

--- doi_to_pmcid.py
+++ doi_to_pmcid.py
@@ -1,20 +1,15 @@
 import requests
+import codeswitch
 
 def main():
-    seed = "https://query.wikidata.org/sparql?format=json&query=SELECT%20%3Fitem%20%3Fdoi%20WHERE%20%7B%0A%20%20%3Fitem%20wdt%3AP356%20%3Fdoi%20.%0A%20%20OPTIONAL%20%7B%20%3Fitem%20wdt%3AP932%20%3Fdummy1%20%7D%0A%20%20FILTER%28%21bound%28%3Fdummy1%29%29%0A%7D"
     url = "https://www.ncbi.nlm.nih.gov/pmc/utils/idconv/v1.0/?format=json&tool=wikidata_worker&email=jamesmhare@gmail.com&ids="
 
-    r = requests.get(seed)
-    blob = r.json()
-
-    wikidata_items = {}  # key: doi; value: wikidata item
     doi = []
-    for result in blob["results"]["bindings"]:
-        wikidata_items[result["doi"]["value"]] = result["item"]["value"].replace("http://www.wikidata.org/entity/", "")
-        doi.append(result["doi"]["value"])
+    for identifier in codeswitch.identifier_generator('P356', 'doi_to_pmcid_list'):
+        if codeswitch.wikidata_to_pmcid(codeswitch.doi_to_wikidata(identifier)) is None:
+            doi.append(identifier)
 
     packages = [doi[x:x+200] for x in range(0, len(doi), 200)]
-
     for package in packages:
         query_string = ""
         for item in package:
@@ -29,10 +24,11 @@ def main():
 
         if "records" in blob:
             for response in blob["records"]:
+                responsedoi = response["doi"].upper()
                 if "pmcid" in response:
-                    print(wikidata_items[response["doi"]] + "\tP932\t\"" + response["pmcid"].replace("PMC", "") + "\"")
+                    print(codeswitch.doi_to_wikidata(responsedoi) + "\tP932\t\"" + response["pmcid"].replace("PMC", "") + "\"")
                     if "pmid" in response:
-                        print(wikidata_items[response["doi"]] + "\tP698\t\"" + response["pmid"] + "\"")
+                        print(codeswitch.doi_to_wikidata(responsedoi) + "\tP698\t\"" + response["pmid"] + "\"")
 
 if __name__ == '__main__':
     main()
